0e4e681049e9ffc98f7962830de263de193b835b
Ruben <15885455+RubenNL@users.noreply.github.com>
2020-04-30 10:34:40 +0200

    modellen 1e versie

--- vpp/export.sh
+++ vpp/export.sh
@@ -5,16 +5,17 @@ vppFile="ipass.vpp"
 vppFilePath="$PWD/$vppFile"
 vppLockPath="$PWD/.$vppFile.lck"
 if ! git diff --exit-code "$vppFilePath"; then
-	rm images/*
+	rm images/* sql/*
 	rm $vppLockPath
 	~/Visual_Paradigm_16.0/scripts/ExportDiagramImage.sh -project "$vppFilePath" -out "$PWD/images" -diagram '*'  -type svg
+	~/Visual_Paradigm_16.0/scripts/GenerateORM.sh -project "$vppFilePath" -out "$PWD/sql" -db 3 -ddl
 	for file in images/*.svg
 	do
 		scour --create-groups --shorten-ids --enable-id-stripping --enable-comment-stripping --remove-metadata --strip-xml-prolog -p 3 "$file" | tee "$file.new" >/dev/null
 		mv "$file.new" "$file"
 		
 	done
-	git add "$vppFilePath" images/*
+	git add "$vppFilePath" images/* sql/*
 	echo "Run again, VPP changed."
 	exit 1
 fi
