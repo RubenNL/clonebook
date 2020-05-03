e9f48aeba6c7f3626ad0fa5a12261ad5784eaf33
Ruben <15885455+RubenNL@users.noreply.github.com>
2020-05-03 11:44:53 +0200

    export.sh geupdate, UseCase.svg geupdate voor PvA

--- vpp/export.sh
+++ vpp/export.sh
@@ -9,12 +9,12 @@ if ! git diff --exit-code "$vppFilePath"; then
 	rm $vppLockPath
 	~/Visual_Paradigm_16.0/scripts/ExportDiagramImage.sh -project "$vppFilePath" -out "$PWD/images" -diagram '*'  -type svg
 	~/Visual_Paradigm_16.0/scripts/GenerateORM.sh -project "$vppFilePath" -out "$PWD/sql" -db 3 -ddl
-	for file in images/*.svg
-	do
-		scour --create-groups --shorten-ids --enable-id-stripping --enable-comment-stripping --remove-metadata --strip-xml-prolog -p 3 "$file" | tee "$file.new" >/dev/null
-		mv "$file.new" "$file"
-		
-	done
+	#for file in images/*.svg
+	#do
+	#	scour --create-groups --shorten-ids --enable-id-stripping --enable-comment-stripping --remove-metadata --strip-xml-prolog -p 3 "$file" | tee "$file.new" >/dev/null
+	#	mv "$file.new" "$file"
+	#	
+	#done
 	git add "$vppFilePath" images/* sql/*
 	echo "Run again, VPP changed."
 	exit 1
