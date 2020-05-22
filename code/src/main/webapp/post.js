8fdddad74bb832705eaec78ca0b868b47e76bf97
Ruben <15885455+RubenNL@users.noreply.github.com>
2020-05-22 15:40:51 +0200

    posts frontend, zonder opvragen

--- code/src/main/webapp/post.js
+++ code/src/main/webapp/post.js
@@ -1,9 +1,37 @@
-function addPost(imgLocation,name,date,text) {
+function addPost(repliedTo,id,imgLocation,name,text) {
 	let node = $('#postTemplate').contents("article").clone();
-	node.find('.profilePicture').attr('img',imgLocation);
-	node.find('.profilePicture').attr('alt',name);
+	node.attr("id",id);
+	node.find('.profilePicture').attr('src',imgLocation);
 	node.find('.name').text(name);
-	node.find('.date').text(date);
 	node.find('.text').text(text);
-	$('#posts').append(node);
+	$(repliedTo?'#'+repliedTo+' > .subReplies':'#posts').append(node);
 }
+var picker=new EmojiButton();
+var lastSelectedField;
+picker.on('emoji',emoji=>lastSelectedField.value+=emoji);
+$(document).on('click','.messageEmoticon',(event)=>{
+	picker.togglePicker(event.target);
+	lastSelectedField=$(event.target).parent().find('.messageBox')[0];
+});
+$(document).on('click','.vote',event=>{
+	sendPost('/rest/post/'+$(event.target).parent().parent().attr('id')+'/vote',{vote:$(event.target).attr('action')});
+});
+$(document).on('click','.replyButton',(event)=>{
+	if($(event.target).parent().parent().find('.messageForm').length>0) $(event.target).parent().parent().find('.messageForm').toggle();
+	else {
+		clone=$('#messageBoxTemplate').contents("div").clone();
+		clone.find('[type="hidden"]').val($(event.target).parent().parent().attr('id'));
+		$(event.target).parent().after(clone);
+	}
+});
+$(document).on('submit','.messageForm',event=>{
+	event.preventDefault();
+	console.log(event);
+	sendPost('/rest/post',event.target);
+});
+addPost(null,"1","icon.png","Voornaam Achternaam","Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.")
+addPost("1","2","icon.png","Voornaam Achternaam","Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.")
+addPost("2","3","icon.png","Voornaam Achternaam","Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.")
+addPost("3","4","icon.png","Voornaam Achternaam","Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.")
+addPost("4","5","icon.png","Voornaam Achternaam","Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.")
+addPost("5","6","icon.png","Voornaam Achternaam","Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.")
--- code/src/main/webapp/tools.js
+++ code/src/main/webapp/tools.js
@@ -15,9 +15,17 @@ function sendGet(path) {
 	}).then(handleResponse);
 }
 function sendPost(path,form) {
+	if(typeof form=="string") var formData=new FormData(document.querySelector(form)).entries();
+	else if(form instanceof HTMLElement) var formData=new FormData(form);
+	else {
+		var formData = new FormData();
+		for ( var key in form ) {
+			formData.append(key, form[key]);
+		}
+	}
 	return fetch(url+path,{
 		method:'POST',
-		body:new URLSearchParams(new FormData(document.querySelector(form)).entries()),
+		body:new URLSearchParams(formData),
 		headers: {
 			'Authorization': 'Bearer ' + window.sessionStorage.getItem("jwt")||""
 		}
