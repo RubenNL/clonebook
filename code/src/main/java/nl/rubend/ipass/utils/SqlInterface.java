edbbf0909451fc1cf1aadc4a2e04ba24a189cdae
Ruben <15885455+RubenNL@users.noreply.github.com>
2020-05-30 14:03:52 +0200

    check of de connectie nog actief is

--- code/src/main/java/nl/rubend/ipass/utils/SqlInterface.java
+++ code/src/main/java/nl/rubend/ipass/utils/SqlInterface.java
@@ -6,7 +6,7 @@ import java.util.Properties;
 
 public class SqlInterface {
 	private static Connection conn;
-	static {
+	private static void connect() {
 		try {
 			Properties prop=new Properties();
 			prop.load(Thread.currentThread().getContextClassLoader().getResourceAsStream("database.properties"));
@@ -16,7 +16,14 @@ public class SqlInterface {
 			e.printStackTrace();
 		}
 	}
+	static {
+		connect();
+	}
 	public static PreparedStatement prepareStatement(String statement) throws SQLException {
+		if(conn.isClosed()) {
+			System.out.println("CONNETION WAS CLOSED");//Om het te vinden in de TomCat logs.
+			connect();
+		}
 		return conn.prepareStatement(statement);
 	}
 }
