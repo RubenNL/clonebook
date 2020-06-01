6406e3ae5dcd85f99e2166b6a6e7874ab98a0ff4
Ruben <15885455+RubenNL@users.noreply.github.com>
2020-06-01 10:46:39 +0200

    betere disconnect detectie, maar kost meer tijd.

--- code/src/main/java/nl/rubend/ipass/utils/SqlInterface.java
+++ code/src/main/java/nl/rubend/ipass/utils/SqlInterface.java
@@ -1,10 +1,10 @@
 package nl.rubend.ipass.utils;
 
+import javax.servlet.ServletContextListener;
 import java.io.IOException;
 import java.sql.*;
 import java.util.Properties;
-
-public class SqlInterface {
+public class SqlInterface implements ServletContextListener {
 	private static Connection conn;
 	private static void connect() {
 		try {
@@ -16,11 +16,14 @@ public class SqlInterface {
 			e.printStackTrace();
 		}
 	}
-	static {
-		connect();
-	}
 	public static PreparedStatement prepareStatement(String statement) throws SQLException {
-		if(conn.isClosed()) {
+		try {
+			PreparedStatement testStatement = conn.prepareStatement("select 1");
+			ResultSet executed = testStatement.executeQuery();
+			executed.next();
+			executed.getInt("1");
+		} catch(NullPointerException | SQLException e) {
+			e.printStackTrace();
 			System.out.println("CONNETION WAS CLOSED");//Om het te vinden in de TomCat logs.
 			connect();
 		}
