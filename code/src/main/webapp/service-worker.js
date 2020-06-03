6a775340680d69548d6d1150390c90a9b2c3310d
Ruben <15885455+RubenNL@users.noreply.github.com>
2020-06-03 10:05:00 +0200

    vergeten spaces->tab te zetten...

--- code/src/main/webapp/service-worker.js
+++ code/src/main/webapp/service-worker.js
@@ -1,18 +1,18 @@
 self.addEventListener('install', (event) => {
-  console.log('service worker installed!');
-  self.skipWaiting();
+	console.log('service worker installed!');
+	self.skipWaiting();
 });
 
 self.addEventListener('activate', (event) => {
-  console.log('service worker activated!');
-  return self.clients.claim();
+	console.log('service worker activated!');
+	return self.clients.claim();
 });
 
 self.addEventListener('fetch', function(event) {
-  if(navigator.onLine) event.respondWith(fetch(event.request));
-  else {
-    let headers=new Headers();
-    headers.append("Content-Type","text/html");
-    event.respondWith(new Response("<h1>Browser is offline.</h1>",{headers:headers}));
-  }
+	if(navigator.onLine) event.respondWith(fetch(event.request));
+	else {
+		let headers=new Headers();
+		headers.append("Content-Type","text/html");
+		event.respondWith(new Response("<h1>Browser is offline.</h1>",{headers:headers}));
+	}
 });
