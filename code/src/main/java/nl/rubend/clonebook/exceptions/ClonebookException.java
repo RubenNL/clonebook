5239ea95186ec0eae457bd425cb304749510fb72
Ruben <15885455+RubenNL@users.noreply.github.com>
2020-06-13 11:29:37 +0200

    meer ipass->clonebook

--- code/src/main/java/nl/rubend/clonebook/exceptions/ClonebookException.java
+++ code/src/main/java/nl/rubend/clonebook/exceptions/ClonebookException.java
@@ -1,6 +1,5 @@
 package nl.rubend.clonebook.exceptions;
 
-public class ClonebookException extends RuntimeException {//extends WebApplicationException {
-	//public IpassException(String errorMessage) {super(Response.status(Response.Status.INTERNAL_SERVER_ERROR).entity(errorMessage).type(MediaType.TEXT_PLAIN).build());}
+public class ClonebookException extends RuntimeException {
 	public ClonebookException(String errorMessage) {super(errorMessage);}
 }
