1f4a5ec2eafe8d604c8fe4f301f5440ac9fb67e2
Ruben <15885455+RubenNL@users.noreply.github.com>
2020-06-16 10:22:53 +0200

    chatten tussen verschillende gebruikers, maar nog zonder account selecteren, of opslag

--- code/src/main/java/nl/rubend/clonebook/websocket/WebSocket.java
+++ code/src/main/java/nl/rubend/clonebook/websocket/WebSocket.java
@@ -2,13 +2,16 @@ package nl.rubend.clonebook.websocket;
 
 import nl.rubend.clonebook.domain.User;
 
+import javax.json.*;
 import javax.websocket.OnClose;
 import javax.websocket.OnMessage;
 import javax.websocket.OnOpen;
 import javax.websocket.Session;
 import javax.websocket.server.PathParam;
 import javax.websocket.server.ServerEndpoint;
+import javax.ws.rs.core.Response;
 import java.io.IOException;
+import java.io.StringReader;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.UUID;
@@ -23,7 +26,7 @@ public class WebSocket {
 		waiting.put(id,user);
 		return id;
 	}
-	public static void sendToUser(User user,String message) {
+	public static int sendToUser(User user,String message) {
 		for(Session session:connected.get(user)) {
 			try {
 				session.getBasicRemote().sendText(message);
@@ -31,6 +34,7 @@ public class WebSocket {
 				e.printStackTrace();
 			}
 		}
+		return connected.get(user).size();
 	}
 	public static void logoutAll(User user) {
 		sendToUser(user,"logoutAll");
@@ -67,9 +71,26 @@ public class WebSocket {
 		try {
 			session.getBasicRemote().sendText("ontvangen:"+message+" van "+user.getName());
 			//message routing is moeilijk.
+			JsonStructure structure = Json.createReader(new StringReader(message)).read();
+			if(structure.getValueType()!= JsonValue.ValueType.OBJECT) return;
+			JsonObject data=(JsonObject) structure;
+			switch(data.getString("type")) {
+				case "chat":
+					chatReceived(user,data);
+					break;
+			}
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
+	}
+	private void chatReceived(User user,JsonObject data) {
+		User dest=User.getUserById(data.getString("dest"));
+		String message=data.getString("message");
+		JsonObjectBuilder builder=Json.createObjectBuilder();
+		builder.add("from",user.getId());
+		builder.add("message",message);
+		builder.add("type","chat");
+		sendToUser(dest,builder.build().toString());
 	}
 	@OnClose
 	public void onClose(@PathParam("id") String id,Session session) {
--- code/src/main/webapp/view/ws.js
+++ code/src/main/webapp/view/ws.js
@@ -1,5 +1,23 @@
 let ws;
-function connectWs() {
-	ws=(new WS(getLoggedInId())).connect();
-	ws.onMessage(message)
+async function connectWs() {
+	$('#page').hide();
+	$('#chatId').text(getLoggedInId());
+	ws = (new WS(getLoggedInId()));
+	await ws.connect();
+	ws.onMessage(onMessage)
+}
+function chat(userId,message) {
+	ws.send({dest:userId,message:message,type:"chat"})
+}
+function onMessage(message) {
+	message=JSON.parse(message);
+	if(message.type!=="chat") return;
+	receivedFromUser(message.from,message.message);
+
+}
+function receivedFromUser(userId,message) {
+	alert('received from '+userId+':'+message);
+}
+function sendMessage() {
+	chat($('#chatDest').val(),$('#chatMessage').val());
 }
